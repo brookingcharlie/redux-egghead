e3c8f8fef9f7fe56289790f1cd9d5049f09bd694
Charlie Brooking <cbrookin@thoughtworks.com>
2016-07-25 23:55:38 +1000

    render example react component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import store from '../src/store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>Increment</button>
+    <button onClick={onDecrement}>Decrement</button>
+  </div>
+);
 
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />,
+    document.getElementById('root')
+  );
 };
 store.subscribe(render);
 
 render();
-document.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT'});
-});
