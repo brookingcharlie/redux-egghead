851f256ec1390dd2f48085ed4331b6840f79c342
Charlie Brooking <cbrookin@thoughtworks.com>
2016-07-26 22:40:35 +1000

    apply reducer composition pattern

--- src/todoReducer.js
+++ src/todoReducer.js
@@ -1,14 +1,24 @@
+// Define reducer for state representing an individual todo object
+const individualTodoReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {id: action.id, text: action.text, completed: false};
+    case 'TOGGLE_TODO':
+      return (state.id === action.id)
+        ? Object.assign({}, state, {completed: !state.completed})
+        : state;
+    default:
+      return state;
+  }
+};
+
+// Apply reducer composition pattern to extent to a list of todo objects
 const todoReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      const todo = {id: action.id, text: action.text, completed: false};
-      return state.concat([todo]);
+      return state.concat([individualTodoReducer(undefined, action)]);
     case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id)
-        ? Object.assign({}, todo, {completed: !todo.completed})
-        : todo
-      );
+      return state.map(t => individualTodoReducer(t, action));
     default:
       return state;
   }
