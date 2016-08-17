33f2374d28882165c085a4c435f956ec1633340e
Charlie Brooking <cbrookin@thoughtworks.com>
2016-08-18 00:33:27 +1000

    test use of FilterLink in TodoApp

--- test/TodoApp.spec.js
+++ test/TodoApp.spec.js
@@ -17,6 +17,7 @@ describe('TodoApp', () => {
       expect(component.find('input')).to.have.length(1);
       expect(component.find('button')).to.have.length(1);
       expect(component.find('li')).to.have.length(0);
+      expect(component.find('FilterLink')).to.have.length(3);
     });
 
     it('for non-empty todo list', () => {
@@ -30,6 +31,21 @@ describe('TodoApp', () => {
       expect(component.find('li').at(1).text()).to.equal('Learn React');
       expect(component.find('li').at(2).hasClass('completed')).to.equal(false);
       expect(component.find('li').at(2).text()).to.equal('Learn Guitar');
+      expect(component.find('FilterLink')).to.have.length(3);
+    });
+  });
+
+  describe('applies filter', () => {
+    it('for empty todo list after filter', () => {
+      const todos = [{id: 0, text: 'Learn Redux', completed: false}];
+      const component = shallow(<TodoApp todos={[]} visibility="SHOW_COMPLETED" />);
+      expect(component.find('li')).to.have.length(0);
+    });
+
+    it('for non-empty todo list after filter', () => {
+      const todos = [{id: 0, text: 'Learn Redux', completed: false}];
+      const component = shallow(<TodoApp todos={todos} visibility="SHOW_ACTIVE" />);
+      expect(component.find('li')).to.have.length(1);
     });
   });
 
@@ -46,5 +62,23 @@ describe('TodoApp', () => {
     const component = shallow(<TodoApp todos={todos} visibility="SHOW_ALL" onToggleTodo={onToggleTodo} />);
     component.find('li').at(1).simulate('click');
     expect(onToggleTodo).to.have.been.called;
+  });
+
+  it('configures filter links', () => {
+    const onSetVisibilityFilter = () => null;
+    const component = shallow(
+      <TodoApp
+        todos={todos}
+        visibility="SHOW_ALL"
+        onSetVisibilityFilter={onSetVisibilityFilter}
+      />
+    );
+    expect(component.find('FilterLink').everyWhere(n =>
+      n.prop('currentFilter') == 'SHOW_ALL' &&
+      n.prop('onClick') == onSetVisibilityFilter
+    )).to.equal(true);
+    expect(component.find('FilterLink').at(0).prop('filter')).to.equal('SHOW_ALL');
+    expect(component.find('FilterLink').at(1).prop('filter')).to.equal('SHOW_ACTIVE');
+    expect(component.find('FilterLink').at(2).prop('filter')).to.equal('SHOW_COMPLETED');
   });
 });
